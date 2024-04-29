700fd9e29c4c2cbbca147641e919aedf1d599f73
Peter <peter@sparf.nl>
2024-04-29 17:10:28 +0200

    Merged all functionality into one app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
+import os
 import streamlit as st
 import openai 
 import pandas as pd
+from loguru import logger
+import extra_streamlit_components as stx
+from PIL import Image
+import requests
+from io import BytesIO
+import base64
 
 st.set_page_config(
     page_title="Skilljourneys ChatGPT",  # Sets the browser tab's title
@@ -9,76 +16,6 @@ st.set_page_config(
     initial_sidebar_state="expanded"  # Optional: use "auto", "expanded", or "collapsed"
 )
 
-# Display the image at the top of the page
-st.image("https://lwfiles.mycourse.app/65a6a0bb6e5c564383a8b347-public/05af5b82d40b3f7c2da2b6c56c24bdbc.png", width=500)
-# Set up the title of the app
-st.title(":rainbow[Skilljourneys ChatGPT]")
-# Link to Trivera Tech website
-st.markdown(":blue[For more information, visit [Triveratech](https://www.triveratech.com).]")
-
-# Get OpenAI API key
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type="password")
-
-# Check if the OpenAI API key is valid
-if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠️')
-else:
-    # Initialize the OpenAI client with the valid API key
-    client = openai.OpenAI(api_key=openai_api_key)
-
-# Allow users to set parameters for the model
-with st.sidebar:
-    st.write("Set Model Parameters")
-    temperature = st.slider("Temperature", 0.0, 2.0, 1.0)
-    max_tokens = st.slider("Max Tokens", 1, 500, 256)
-    top_p = st.slider("Top P", 0.0, 1.0, 1.0)
-    frequency_penalty = st.slider("Frequency Penalty", 0.0, 2.0, 0.0)
-    presence_penalty = st.slider("Presence Penalty", 0.0, 2.0, 0.0)
-
-# Initialize session state variables if they don't exist
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display previous messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# Check for client initialization before displaying the input field
-if locals() or 'client' in globals():  
-    # Input for new message
-    if prompt := st.chat_input("Enter Prompt"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        with st.spinner("Getting a response..."):
-            try:
-                with st.chat_message("assistant"):
-                    # Generate the response from the model
-                    stream = client.chat.completions.create(
-                        model=st.session_state["openai_model"],
-                        messages=[
-                            {"role": m["role"], "content": m["content"]}
-                            for m in st.session_state.messages
-                        ],
-                        temperature=temperature, 
-                        max_tokens=max_tokens,
-                        top_p=top_p,
-                        frequency_penalty=frequency_penalty,
-                        presence_penalty=presence_penalty,
-                        stream=True,
-                    )
-                    response = st.write_stream(stream)
-                st.session_state.messages.append({"role": "assistant", "content": response})    
-            except Exception as e:
-                st.error(f"An unexpected error occurred: {str(e)}", icon='🚨')               
-                
-if st.session_state.messages:
-    if st.button('Clear', key="clear"):
-        st.session_state.messages = []
-        st.rerun()  
-
-# Function to create and cache the model data table
 @st.cache_data
 def create_model_data_table():
     model_data = {
@@ -108,67 +45,205 @@ def create_model_data_table():
             "32,768 tokens",
             "16,385 tokens",
             "16,385 tokens"
-        ],
-        "TRAINING DATA": [
-            "Up to Dec 2023",
-            "Up to Dec 2023",
-            "Up to Apr 2023",
-            "Up to Sep 2021",
-            "Up to Sep 2021",
-            "Up to Sep 2021",
-            "Up to Sep 2021"
         ]
     }
     return pd.DataFrame(model_data)
 
-with st.sidebar:
-    # Code to display the model data table within an expander
-    with st.expander("Model Information"):
-        df_models = create_model_data_table()
-        st.table(df_models)
+def is_api_key_valid(api_key):
+    try:
+       client = openai.OpenAI(api_key=api_key)
+       client.models.list()
+    except openai.AuthenticationError as e:
+        return False
+    else:
+        return True
 
-    # Data policy and model endpoint compatibility expander
-    with st.expander("OpenAI API Data Usage Policy"):
-        st.markdown("""
-        **How we use your data**
-        Your data is your data.
+# Display the image at the top of the page
+st.image("https://lwfiles.mycourse.app/65a6a0bb6e5c564383a8b347-public/05af5b82d40b3f7c2da2b6c56c24bdbc.png", width=500)
 
-        As of March 1, 2023, data sent to the OpenAI API will not be used to train or improve OpenAI models (unless you explicitly opt in). One advantage to opting in is that the models may get better at your use case over time.
+# Get OpenAI API key
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type="password")
 
-        To help identify abuse, API data may be retained for up to 30 days, after which it will be deleted (unless otherwise required by law). For trusted customers with sensitive applications, zero data retention may be available. With zero data retention, request and response bodies are not persisted to any logging mechanism and exist only in memory in order to serve the request.
+# Check if the OpenAI API key is valid
+if not openai_api_key:
+    st.warning('Please enter your OpenAI API key!', icon='⚠️')
+    st.stop();
+# check if the API key is not valid
+if openai_api_key and not is_api_key_valid(openai_api_key):
+    st.warning('Invalid OpenAI API key. Please provide a valid key.', icon='⚠️')
+    st.stop()
+else:
+    client = openai.OpenAI(api_key=openai_api_key)    
 
-        Note that this data policy does not apply to OpenAI's non-API consumer services like ChatGPT or DALL·E Labs.
+chosen_id = stx.tab_bar([
+    stx.TabBarItemData(id="1", title="Chat", description=""),
+    stx.TabBarItemData(id="2", title="Image Analysis", description=""),
+    stx.TabBarItemData(id="3", title="Image Generation", description="")
+])
 
-        **Default usage policies by endpoint**
+if chosen_id == "1":
+    # Set up the title of the app
+    st.title(":rainbow[Skilljourneys ChatGPT]")
+    # Link to Trivera Tech website
+    st.markdown(":blue[For more information, visit [Triveratech](https://www.triveratech.com).]")
+    # Allow users to set parameters for the model
+    with st.sidebar:
+        st.write("Set Model Parameters")
+        temperature = st.slider("Temperature", 0.0, 2.0, 1.0)
+        max_tokens = st.slider("Max Tokens", 1, 500, 256)
+        top_p = st.slider("Top P", 0.0, 1.0, 1.0)
+        frequency_penalty = st.slider("Frequency Penalty", 0.0, 2.0, 0.0)
+        presence_penalty = st.slider("Presence Penalty", 0.0, 2.0, 0.0)
+            # Function to create and cache the model data table
+    
+    with st.sidebar:
+        # Code to display the model data table within an expander
+        with st.expander("Model Information"):
+            df_models = create_model_data_table()
+            st.table(df_models)
 
-        | ENDPOINT | DATA USED FOR TRAINING | DEFAULT RETENTION | ELIGIBLE FOR ZERO RETENTION |
-        | --- | --- | --- | --- |
-        | /v1/chat/completions | No | 30 days | Yes, except image inputs |
-        | /v1/files | No | Until deleted by customer | No |
-        | ... | ... | ... | ... |
-        | /v1/completions | No | 30 days | Yes |
-        * Image inputs via the gpt-4-vision-preview model are not eligible for zero retention.
+    # Allow users to select the model
+    model_options = list(df_models["MODEL"])
+    # Find the index of 'gpt-3.5-turbo' in the model options list
+    default_index = model_options.index('gpt-3.5-turbo') if 'gpt-3.5-turbo' in model_options else 0
+    selected_model = st.sidebar.radio("Select the OpenAI model", model_options, index=default_index)
 
-        * For the Assistants API, we are still evaluating the default retention period during the Beta. We expect that the default retention period will be stable after the end of the Beta.
+    # Update the model based on user selection
+    st.session_state["openai_model"] = selected_model
 
-        **Model endpoint compatibility**
+    # Initialize session state variables if they don't exist
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-        | ENDPOINT | LATEST MODELS |
-        | --- | --- |
-        | /v1/assistants | All models except gpt-3.5-turbo-0301 supported. |
-        | ... | ... |
-        This list excludes all of our deprecated models.
+    # Display previous messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-        For details, see our API data usage policies. To learn more about zero retention, get in touch with our sales team.
-        """, unsafe_allow_html=True)
+    # Input for new message
+    if prompt := st.chat_input("Enter Prompt"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.spinner("Getting a response..."):
+            try:
+                with st.chat_message("assistant"):
+                    # Generate the response from the model
+                    stream = client.chat.completions.create(
+                        model=st.session_state["openai_model"],
+                        messages=[
+                            {"role": m["role"], "content": m["content"]}
+                            for m in st.session_state.messages
+                        ],
+                        temperature=temperature, 
+                        max_tokens=max_tokens,
+                        top_p=top_p,
+                        frequency_penalty=frequency_penalty,
+                        presence_penalty=presence_penalty,
+                        stream=True,
+                    )
+                    response = st.write_stream(stream)
+                st.session_state.messages.append({"role": "assistant", "content": response})    
+            except Exception as e:
+                st.error(f"An unexpected error occurred: {str(e)}", icon='🚨')               
+                    
+    if st.session_state.messages:
+        if st.button('Clear', key="clear"):
+            st.session_state.messages = []
+            st.rerun()  
+
+#Image generation
+if chosen_id == "3":
+        # Set up the title of the app
+    st.title(":rainbow[Skilljourneys Vision GPT]")
+    st.subheader(":rainbow[Creating images]")
+    # Link to Trivera Tech website
+    st.markdown(":blue[For more information, visit [Triveratech](https://www.triveratech.com).]")
+
+    with st.sidebar:
+       model_option = st.selectbox("Select Model", ["gpt-4-vision-preview", "gpt-4-1106-vision-preview"], index=0)
+       # DALL-E settings
+       dalle_size = st.selectbox("Select Image Size", ["1024x1024", "512x512", "256x256"], index=0)
+       dalle_quality = st.selectbox("Select Image Quality", ["standard", "hd"], index=0)
+       dalle_n = st.slider("Number of Images", 1, 4, 1)
+    
+
+    dalle_prompt = st.text_area(label="Enter Prompt for DALL-E", value="a beautiful holiday destination")
+    if st.button("Generate Image"):
+        if openai_api_key:
+            try:
+                # Request to OpenAI DALL-E
+                response = client.images.generate(
+                     prompt=dalle_prompt,
+                    size=dalle_size,
+                    quality=dalle_quality,
+                    n=dalle_n,
+                )
+                # Display generated images
+                for i in range(len(response.data)):
+                    st.image(response.data[i].url, caption=f"Generated Image {i+1}")
+            except Exception as e:
+                st.error(f"Error generating images with DALL-E: {e}")
+        else:
+            st.warning("Please enter the OpenAI API key.")
 
 
+# Function to get image URL from uploaded file
+def get_image_url(image, image_type):
+    buffer = BytesIO()
+    image.save(buffer, format=image_type)
+    buffer.seek(0)
+    data_uri = base64.b64encode(buffer.read()).decode('utf-8')
+    image_url = f"data:image/{image_type};base64,{data_uri}"
+    return image_url
 
-# Allow users to select the model
-model_options = list(df_models["MODEL"])
-# Find the index of 'gpt-3.5-turbo' in the model options list
-default_index = model_options.index('gpt-3.5-turbo') if 'gpt-3.5-turbo' in model_options else 0
-selected_model = st.sidebar.radio("Select the OpenAI model", model_options, index=default_index)
+if chosen_id == "2":   
+            # Set up the title of the app
+    st.title(":rainbow[Skilljourneys Vision GPT]")
+    st.subheader(":rainbow[Upload an Image for Analysis]")
 
-# Update the model based on user selection
-st.session_state["openai_model"] = selected_model
+    # Link to Trivera Tech website
+    st.markdown(":blue[For more information, visit [Triveratech](https://www.triveratech.com).]")
+
+    with st.sidebar:
+        uploaded_image = st.file_uploader("Upload file", type=["jpg", "jpeg", "png", "gif"])
+        model_option = st.selectbox("Select Model", ["gpt-4-vision-preview", "gpt-4-1106-vision-preview"], index=0) 
+
+    user_prompt = st.text_area(label="Enter Prompt", value="What's in this image?")
+
+    if st.button(label="Analyze Image"):
+        if openai_api_key and (uploaded_image):
+            image_to_analyze = None
+
+            if uploaded_image:
+                try:
+                    with Image.open(uploaded_image) as image_to_analyze:
+                       image_type = image_to_analyze.format
+                       # Display and process the uploaded image
+                       st.image(image_to_analyze, caption='Uploaded Image',)
+                       image_url = get_image_url(image_to_analyze, image_type)
+                except (FileNotFoundError, IOError):
+                       image_type = None              
+
+            if image_url:
+                # Request to OpenAI
+                response = client.chat.completions.create(
+                    model=model_option,
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": [{"type": "text", "text": user_prompt},
+                                        {"type": "image_url", "image_url": {"url": image_url}}],
+                        }
+                    ],
+                    max_tokens=300,
+                )
+                try:
+                    # Extract the response text correctly according to the response structure
+                    result_text = response.choices[0].message.content
+                    st.write(result_text)
+                except KeyError as e:
+                    st.error(f"Error extracting response: {e}")
+                    st.write(response)  # Print the whole response for debugging
+        else:
+            st.warning("Please provide an image URL or upload an image and ensure the API key is entered.")
